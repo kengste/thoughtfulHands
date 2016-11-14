88d40b0904aac4e0a982743450f069b09d9a268a
kengste <koh.kenghong@outlook.com>
2016-11-14 12:25:24 +0800

    amended seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#
-# Beneficiary.create([
-#  { name: 'Action For AIDS (Singapore)' },
-#  { name: 'Agency for Animal Welfare Ltd' },
-#  { name: 'Alzheimers Disease Association' },
-#  { name: 'Art Therapists Association Singapore' },
-#  { name: 'Beyond Social Services' },
-#  { name: 'Breast Cancer Foundation' },
-#  { name: 'Childrens Cancer Foundation' },
-#  { name: 'Childrens Charities Association Of Singapore, The' },
-#  { name: 'Guide Dogs Association of the Blind Ltd' },
-#  { name: 'HOPE Dog Rescue' },
-#  { name: 'Humanitarian Organization for Migration Economics' },
-#  { name: 'Association Of Women For Action And Research' },
-#  { name: 'Muscular Dystrophy Association (Singapore)' },
-#  { name: 'National Arthritis Foundation' },
-#  { name: 'Oasis Second Chance Animal Shelter – OSCAS' },
-#  { name: 'Singapore Association For The Deaf, The' },
-#  { name: 'Save Our Street Dogs (SOSD)' },
-#  { name: 'SPCA Singapore' },
-#  { name: 'Transient Workers Count Too (TWC2)' }
-# ])
+
+Beneficiary.create([
+ { name: 'Action For AIDS (Singapore)' },
+ { name: 'Agency for Animal Welfare Ltd' },
+ { name: 'Alzheimers Disease Association' },
+ { name: 'Art Therapists Association Singapore' },
+ { name: 'Beyond Social Services' },
+ { name: 'Breast Cancer Foundation' },
+ { name: 'Childrens Cancer Foundation' },
+ { name: 'Childrens Charities Association Of Singapore, The' },
+ { name: 'Guide Dogs Association of the Blind Ltd' },
+ { name: 'HOPE Dog Rescue' },
+ { name: 'Humanitarian Organization for Migration Economics' },
+ { name: 'Association Of Women For Action And Research' },
+ { name: 'Muscular Dystrophy Association (Singapore)' },
+ { name: 'National Arthritis Foundation' },
+ { name: 'Oasis Second Chance Animal Shelter – OSCAS' },
+ { name: 'Singapore Association For The Deaf, The' },
+ { name: 'Save Our Street Dogs (SOSD)' },
+ { name: 'SPCA Singapore' },
+ { name: 'Transient Workers Count Too (TWC2)' }
+])
 
 User.create([
   {first_name: 'Larry', family_name: 'David', is_admin: true, email: 'larry@gmail.com', password: 'password', },
